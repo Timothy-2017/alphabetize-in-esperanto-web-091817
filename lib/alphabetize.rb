edad6d040067692c5503159836e95e22e619d02d
Tim Eubank <timothy.eubank@gmail.com>
2017-09-15 19:55:47 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  sorted = []
+  sorted = arr.sort_by { |string| string.split("").map{ |char| ESPERANTO_ALPHABET.index(char)}}
+  sorted
 end
